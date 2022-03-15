a458b4289fa0f316544b8a28c2eab418fd4a2d69
Thomas Clifford <tclifford5225@gmail.com>
2022-03-14 22:26:35 -0400

    Create Files for Part 2

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ How to run:
 
 filesToSend.txt contains a list of the files that the client will send to the server. Each file is separated by a newline character.
 
-In TCPServer.java, line 20 is a for loop. This loop accepts a connection for every new file in filesToSend. 
+In part1.TCPServerPart1.java, line 20 is a for loop. This loop accepts a connection for every new file in filesToSend. 
 The number in the for loop should correspond to the number of files in filesToSend.txt. 
 I have not found a way to automate this yet, so that is why it is hard coded. For instance, if there are 3 files in filesToSend.txt:
 
@@ -12,8 +12,8 @@ for (int i = 0; i < 3; i++)
 
 Each of the 3 programs should be run on their own machine. This way, each of the socket connections have their own IP address.
 
-Start TCPServerRouter.java. This will facilitate connections between the server and the router
-Start TCPServer.java. This will accept connection from TCPClient.java through TCPServerRouter.javva
-Start TCPClient.java. This will send data to TCPServer.java through TCPServerRouter.java.
+Start part1.TCPServerRouterPart1.java. This will facilitate connections between the server and the router
+Start part1.TCPServerPart1.java. This will accept connection from part1.TCPClientPart1.java through part1.TCPServerRouterPart1.javva
+Start part1.TCPClientPart1.java. This will send data to part1.TCPServerPart1.java through part1.TCPServerRouterPart1.java.
 
 
---
+++ src/part2/TCPServerRouter2Part2.java
@@ -0,0 +1,40 @@
+package part2;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TCPServerRouter2Part2 {
+    public static void main(String[] args) throws IOException {
+        Object [][] routingTable = new Object [10000][2]; // routing table
+        int routerIndex = 0; // index in the routing table
+
+        Socket clientSocket = null; // socket for the thread
+        final int PORT = 5555; // port number
+        ServerSocket serverSocket = null; // server socket for accepting connections
+        try {
+            serverSocket = new ServerSocket(PORT);
+            System.out.println("ServerRouter is Listening on port: " + PORT);
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: " + PORT);
+            System.exit(1);
+        }
+
+        // Creating threads with accepted connections
+        while (true) {
+            try {
+                clientSocket = serverSocket.accept();
+                SThreadPart2 clientThread = new SThreadPart2(routingTable, clientSocket, routerIndex); // creates a thread with a random port
+                clientThread.start(); // starts the thread
+                routerIndex++; // increments the index
+                System.out.println("ServerRouter connected with Client/Server: " + clientSocket.getInetAddress().getHostAddress());
+            } catch (IOException e) {
+                System.err.println("Client/Server failed to connect.");
+                // closing connections
+                clientSocket.close();
+                serverSocket.close();
+                System.exit(1);
+            }
+        }
+    }
+}
